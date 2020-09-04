ccb76f1c64876d26675560066d17c868590ef642
WeiNyn <nguyenhuy17798@gmail.com>
2020-09-04 09:26:54 +0700

    add install docker-compose

--- install_script.sh
+++ install_script.sh
@@ -4,4 +4,7 @@ sudo apt-get remove docker docker-engine docker.io
 sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo curl -L https://github.com/docker/compose/releases/download/1.27.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 docker --version
+docker-compose --version
